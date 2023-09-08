bc9128a8f47b6281d073c204258a85cb5927758c
Teta_Lena <tetalenaa@gmail.com>
2023-09-08 13:16:45 +0200

    tailwind and nav component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Music</li>
+          <li>Videos</li>
+          <li>Biography</li>
+          <li>Store</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
