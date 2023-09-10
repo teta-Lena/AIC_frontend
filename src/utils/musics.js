3353a7745e6ca77d206e8afddf0978878daa1d73
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-09-10 17:29:15 +0200

    feat: youtube music

---
+++ src/utils/musics.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 1,
+    title: "Away",
+    ytId: "16yEVs2g9PQ",
+    description:
+      "Listen to the official audio for Away by Ariel Ways ft Juno Kizigenza. Prod by Rexxie",
+    released: "2020",
+    thumbnail: "https://i.ytimg.com/vi/16yEVs2g9PQ/hqdefault.jpg",
+  },
+  {
+    id: 2,
+    title: "Shayo",
+    ytId: "L2YXScSTsHg",
+    description:
+      "Listen to the official audio for Shayo by Ariel Ways. Prod by Rexxie",
+    released: "2023",
+  },
+  {
+    id: 3,
+    title: "You Should Know",
+    ytId: "S83KpnM5cSY",
+    description:
+      "Listen to the official audio for You Should Know by Ariel Ways. Prod by Rexxie",
+    released: "2022",
+  },
+  {
+    id: 4,
+    title: "Good Luck",
+    ytId: "diHGS8fhllg",
+    description:
+      "Listen to the official audio for Good Luck by Ariel Ways. Prod by Rexxie",
+    released: "2022",
+  },
+];
