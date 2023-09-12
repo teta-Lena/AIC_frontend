ba6b526bdf3c757d8e57e96be3714a0c63327dfb
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-09-12 08:05:11 +0200

    chore: optimize video contents

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import JoinLive from "./pages/live/JoinLive";
 import Live from "./pages/live/Live";
 import NotFound from "./pages/404/NotFound";
 import Videos from "./components/videos/Videos";
+import VideosPage from "./pages/videos";
 
 function App() {
   function PrivateRoutes() {
@@ -37,7 +38,7 @@ function App() {
           <Route path="/dashboard" element={<Home />} />
         </Route>
         <Route path="/" element={<Homepage />} />
-        <Route path="/videos" element={<Videos/>}  />
+        <Route path="/videos" element={<VideosPage/>}  />
         <Route path="/music" element={<MusicIndex />} />
         <Route path="/music/:id" element={<MusicIdPage />} />
         <Route path="/bio" element={<Bio />} />
--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import './Videos.css'
-import pic6 from '../../assets/pic6.jpg'
-import pic8 from '../../assets/pic8.jpg'
-import pic7 from '../../assets/pic7.jpg'
-import pic15 from '../../assets/pic15.jpg'
-import { BsPlay } from 'react-icons/bs'
-import Navbar from '../shared/navbar'
-import MidNav from '../shared/mid-nav'
-import Footer from '../shared/footer'
 import MainLayout from '@/layouts/MainLayout'
+import { BsPlay } from 'react-icons/bs'
+import pic15 from '../../assets/pic15.jpg'
+import pic6 from '../../assets/pic6.jpg'
+import pic7 from '../../assets/pic7.jpg'
+import pic8 from '../../assets/pic8.jpg'
+import './Videos.css'
 
 
 const Videos = () => {
@@ -16,8 +12,8 @@ const Videos = () => {
     return (
         <MainLayout >
             <div className='main-container'>
-                {videos.map(video => (<div className='wrapper'>
-                    <img className='image' src={pic6} alt="" />
+                {videos.map((video, i) => (<div key={i} className='wrapper'>
+                    <img  className='image' src={pic6} alt="" />
                     <div className="overlay">
                         <h2>shayo {video}</h2>
                         <div className="watch">
@@ -28,7 +24,7 @@ const Videos = () => {
 
                 </div>))}
 
-                {videos.map(video => (<div className='wrapper'>
+                {videos.map((video, i) => (<div key={i} className='wrapper'>
                     <img className='image' src={pic15} alt="" />
                     <div className="overlay">
                         <h2>away {video}</h2>
@@ -40,7 +36,7 @@ const Videos = () => {
 
                 </div>))}
 
-                {videos.map(video => (<div className='wrapper'>
+                {videos.map((video, i) => (<div key={i} className='wrapper'>
                     <img className='image' src={pic7} alt="" />
                     <div className="overlay">
                         <h2>you should know {video}</h2>
@@ -51,7 +47,7 @@ const Videos = () => {
                     </div>
                 </div>))}
 
-                {videos.map(video => (<div className='wrapper'>
+                {videos.map((video, i) => (<div key={i} className='wrapper'>
                     <img className='image' src={pic8} alt="" />
                     <div className="overlay">
                         <h2>bad {video}</h2>
